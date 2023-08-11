84649a0ad2632d34decd999e49e50c038bff065b
Akmal <akmal15051993@gmail.com>
2023-08-12 04:57:24 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,147 @@
+<script>
+  import mainPage from "./components/mainPage.vue"
+
+  export default {
+    name: "App",
+    components: {
+      mainPage
+    }
+  }
+</script>
+
+<template>
+  <mainPage/>
+</template>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "inter";
+  }
+
+  .header {
+    max-width: 1440px;
+    background-color: #FAFAFA;
+    margin: 0 auto;
+    padding-bottom: 33px;
+  }
+
+  .header__container {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+
+  .container {
+    max-width: 980px;
+    margin: 0 auto;
+  }
+
+  .header__content {
+    display: flex;
+    justify-content: space-between;
+    padding: 43.5px 0;
+  }
+
+  .header__nav_list {
+    display: flex;
+    align-items: center;
+  }
+
+  .header__nav-items {
+    margin-left: 10px;
+    list-style: none;
+    margin-right: 10px;
+  }
+
+  .header__icons {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .header-name {
+    text-align: center;
+    font-weight: 400;
+  }
+
+  /* Main */
+
+  .main {
+    max-width: 1440px;
+    margin: 0 auto;
+    padding-top: 32px;
+  }
+
+  .main__content_header {
+    display: flex;
+    align-items: center;
+    background-color: #FAFAFA;
+    padding-bottom: 16px;
+  }
+
+  .main-image {
+    width: 44px;
+    height: 44px;
+    margin-right: 14px;
+  }
+
+  .main__header-title{
+    font-size: 18px;
+    font-weight: 700;
+  }
+
+  .main__content {
+    border-radius: 10px;
+    border: 1px solid #F1F1F1;
+    background: #FFF;
+    box-shadow: 0px 4px 40px 0px rgba(0, 0, 0, 0.07);
+    padding: 24px 20px;
+  }
+
+  .main__content_description {
+    margin-bottom: 32px;
+  }
+
+  .main-title {
+    margin-bottom: 15px;
+    color: #292929;
+    font-size: 26px;
+    font-weight: 700;
+  }
+
+  .main-subtitle {
+    color: var#404040;
+    font-size: 14px;
+    font-weight: 400;
+  }
+
+  .main-subtitle span {
+    font-weight: 700;
+  }
+
+  .main__content_info{
+    display: flex;
+  }
+
+  .issues__description {
+    display: flex;
+  }
+
+  .main__issues {
+    padding: 0 10px;
+  }
+
+  .content-date {
+    text-transform: uppercase;
+  }
+
+  .issues__description{
+    padding-bottom: 6px;
+  }
+
+  .issues__description:nth-last-child() {
+    padding: 0;
+  }
+</style>
+
---
+++ src/components/mainPage.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="header">
+        <div class="header__container">
+            <div class="header__content">
+                <img src="../assets/images/logo.png" class="header-logo">
+
+                <nav class="header__nav">
+                    <ul class="header__nav_list">
+                        <li class="header__nav-items">
+                            <a href="#" class="header__nav-links">
+                                <img src="../assets/images/home.png" alt="" class="header__nav-icon">
+                            </a>
+                        </li>
+                        
+                        <li class="header__nav-items">
+                            <a href="#" class="header__nav-links">
+                                <img src="../assets/images/photo.png" alt="" class="header__nav-icon">
+                            </a>
+                        </li>
+                        
+                        <li class="header__nav-items">
+                            <a href="#" class="header__nav-links">
+                                <img src="../assets/images/shape.png" alt="" class="header__nav-icon">
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="header__icons">
+                <div class="header__icons_content">
+                    <img src="../assets/images/img1.png" alt="" class="header-icon">
+
+                    <p class="header-name">Josh</p>
+                </div>
+                
+                <div class="header__icons_content">
+                    <img src="../assets/images/img2.png" alt="" class="header-icon">
+
+                    <p class="header-name">Andrew</p>
+                </div>
+
+                <div class="header__icons_content">
+                    <img src="../assets/images/img3.png" alt="" class="header-icon">
+
+                    <p class="header-name">Camille</p>
+                </div>
+
+                <div class="header__icons_content">
+                    <img src="../assets/images/img4.png" alt="" class="header-icon">
+
+                    <p class="header-name">Camille</p>
+                </div>
+
+                <div class="header__icons_content">
+                    <img src="../assets/images/img5.png" alt="" class="header-icon">
+
+                    <p class="header-name">Piter</p>
+                </div>
+
+                <div class="header__icons_content">
+                    <img src="../assets/images/img6.png" alt="" class="header-icon">
+
+                    <p class="header-name">Can</p>
+                </div>
+
+                <div class="header__icons_content">
+                    <img src="../assets/images/img7.png" alt="" class="header-icon">
+
+                    <p class="header-name">Iloveanime</p>
+                </div>
+
+                <div class="header__icons_content">
+                    <img src="../assets/images/img8.png" alt="" class="header-icon">
+
+                    <p class="header-name">Diself</p>
+                </div>
+
+                <div class="header__icons_content">
+                    <img src="../assets/images/img9.png" alt="" class="header-icon">
+
+                    <p class="header-name">Gartor</p>
+                </div>
+
+                <div class="header__icons_content">
+                    <img src="../assets/images/img10.png" alt="" class="header-icon">
+
+                    <p class="header-name">Camilr</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <main class="main">
+         <div class="container">
+            <div class="main__content_header">
+                <img src="../assets/images/img2.png" alt="" class="main-image">
+                <p class="main__header-title">joshua_l</p>
+            </div>
+
+            <div class="main__content">
+                
+                <div class="main__content_description">
+                    <h1 class="main-title">Vue.js</h1>
+                    <div class="main-subtitle"><span>JavaScript</span> framework for building interactive web applications</div>
+                </div>
+
+                <div class="main__content_info">
+                    <div class="main-star">Star</div>
+                    <div class="main-subscribers">156k</div>
+                    <div class="main-fork">Fork</div>
+                    <div class="main-views">4</div>
+                </div>
+            </div>
+
+            <div class="main__issues">
+                    <h2 class="issues-title">Hide issues</h2>
+                    <div class="issues__content">
+                        <div class="issues__description">
+                            <p class="issues-name">joshua_l</p>
+                            <p class="issues-subtitle">Enable performance measuring in production, at the user's request</p>
+                        </div>
+
+                        <div class="issues__description">
+                            <p class="issues-name">Camille</p>
+                            <p class="issues-subtitle">It's Impossible to Rename an Inherited Slot</p>
+                        </div>
+
+                        <div class="issues__description">
+                            <p class="issues-name">Marselle</p>
+                            <p class="issues-subtitle">transition-group with flex parent causes removed items to fly</p>
+                        </div>
+                    </div>
+
+                    <div class="content-date">15 may</div>
+                </div>
+         </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "mainPage"
+    }
+</script>
